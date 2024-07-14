11a47cee07ff0403c58e09f4e199d7f9af5cb16b
aman8710 <mishraman8710@gmail.com>
2024-07-15 03:57:18 +0530

    some examples

---
+++ Desktop/c++ love babbar/function1.cpp
@@ -0,0 +1,17 @@
+// functions to print anything n times
+#include <iostream>
+using namespace std;
+
+
+void printLine() {
+    for(int i=0; i<10; i++){
+        cout << "Aman" << endl;
+    }
+}
+
+int main() {
+
+    
+printLine();
+return 0;
+}
