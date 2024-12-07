f33401b3ff6d0578146297b1502668cc1959ba61
Aman mishra <139130126+devamanmishra@users.noreply.github.com>
2024-12-07 18:38:08 +0530

    Delete Desktop/c++ love babbar directory

--- Desktop/c++ love babbar/lecture1a.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-int num = 50;
-cout << num <<endl;
-cout << sizeof(num) << endl;
-return 0;
-}
--- Desktop/c++ love babbar/lecture2b.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-  for(int i=10;i<=12;i=i+1){
-    cout<<"Monica my darling" <<endl;
-  }
-    return 0;
-}
